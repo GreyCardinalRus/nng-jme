445227c11d377409f8ffec6a4960aad6d0fbaa7c
grey c rus <grey.c.rus@gmail.com>
2011-02-07 11:55:01 +0300

    booth Ninja anim random (press "space")

--- src/gctest/NinjaAnimation.java
+++ src/gctest/NinjaAnimation.java
@@ -1,6 +1,6 @@
 package gctest;
 
-
+import java.lang.reflect.Array;
 
 import com.jme3.animation.AnimChannel;
 import com.jme3.animation.AnimControl;
@@ -16,133 +16,156 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
 import com.jme3.scene.debug.SkeletonDebugger;
- 
-/** Sample 7 - how to load an OgreXML model and play an animation, 
- * using channels, a controller, and an AnimEventListener. */
-public class NinjaAnimation extends SimpleApplication
-  implements AnimEventListener, ActionListener {
- 
-    private AnimChannel channel1, channel2;
-//    private String[] animNames;
+
+/**
+ * Sample 7 - how to load an OgreXML model and play an animation, using
+ * channels, a controller, and an AnimEventListener.
+ */
+public class NinjaAnimation extends SimpleApplication implements
+		AnimEventListener, ActionListener {
+
+	private AnimChannel channel1, channel2;
+	private String[] animNames;
+	private int animNumbers;
 
 	private AnimChannel channel;
-  private AnimControl control;
-  Node player;
- 
-  public static void main(String[] args) {
-	  NinjaAnimation app = new NinjaAnimation();
-    app.start();
-  }
- 
-  @Override
-  public void simpleInitApp() {
-    viewPort.setBackgroundColor(ColorRGBA.LightGray);
-    initKeys();
-    flyCam.setMoveSpeed(100f);
-    cam.setLocation( new Vector3f( 0f, 150f, -325f ) );
-    cam.lookAt( new Vector3f( 0f, 100f, 0f ), Vector3f.UNIT_Y );
+	private AnimControl control;
+	Node player;
 
-    DirectionalLight dl = new DirectionalLight();
-    dl.setDirection(new Vector3f(-0.1f, -1f, 1).normalizeLocal());
-    rootNode.addLight(dl);
-    Node model1 = (Node) assetManager.loadModel("Models/Ninja/Ninja.mesh.xml");
-    Node model2 = (Node) assetManager.loadModel("Models/Ninja/Ninja.mesh.xml");
-//    Node model2 = model1.clone();
+	public static void main(String[] args) {
+		NinjaAnimation app = new NinjaAnimation();
+		app.start();
+	}
 
-    model1.setLocalTranslation(-60, 0, 0);
-    model2.setLocalTranslation(60, 0, 0);
-    
-    // rotate
-    model1.rotate(0,-1.7f, 0);
-    model2.rotate(0,1.7f, 0);
-    //\
-    AnimControl control1 = model1.getControl(AnimControl.class);
-    
- //   for (String anim : control1.getAnimationNames()) { System.out.println(anim); }
-    
-  //  animNames = control1.getAnimationNames().toArray(new String[0]);
-    channel1 = control1.createChannel();
-    
-    AnimControl control2 = model2.getControl(AnimControl.class);
-    channel2 = control2.createChannel();
+	@Override
+	public void simpleInitApp() {
+		viewPort.setBackgroundColor(ColorRGBA.LightGray);
+		initKeys();
+		flyCam.setMoveSpeed(100f);
+		cam.setLocation(new Vector3f(0f, 150f, -325f));
+		cam.lookAt(new Vector3f(0f, 100f, 0f), Vector3f.UNIT_Y);
 
-    SkeletonDebugger skeletonDebug = new SkeletonDebugger("skeleton1", control1.getSkeleton());
-    Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-    mat.getAdditionalRenderState().setWireframe(true);
-    mat.setColor("Color", ColorRGBA.Green);
-    mat.getAdditionalRenderState().setDepthTest(false);
-    skeletonDebug.setMaterial(mat);
-    model1.attachChild(skeletonDebug);
+		DirectionalLight dl = new DirectionalLight();
+		dl.setDirection(new Vector3f(-0.1f, -1f, 1).normalizeLocal());
+		rootNode.addLight(dl);
+		Node model1 = (Node) assetManager
+				.loadModel("Models/Ninja/Ninja.mesh.xml");
+		Node model2 = (Node) assetManager
+				.loadModel("Models/Ninja/Ninja.mesh.xml");
+		// Node model2 = model1.clone();
 
-    skeletonDebug = new SkeletonDebugger("skeleton2", control2.getSkeleton());
-    skeletonDebug.setMaterial(mat);
-    model2.attachChild(skeletonDebug);
+		model1.setLocalTranslation(-60, 0, 0);
+		model2.setLocalTranslation(60, 0, 0);
 
-    rootNode.attachChild(model1);
-    rootNode.attachChild(model2);
+		// rotate
+		model1.rotate(0, -1.7f, 0);
+		model2.rotate(0, 1.7f, 0);
+		// \
+		AnimControl control1 = model1.getControl(AnimControl.class);
+//		System.out.print(control1.getAnimationNames().size());
+		//animNames = (String[]) control1.getAnimationNames().toArray();
+		animNumbers=0;
+		 animNames = new String[control1.getAnimationNames().size()+1];
+		 for (String anim : control1.getAnimationNames()) {
+		 animNames[animNumbers++]=anim; }
 
-//    player = (Node) assetManager.loadModel("Models/Oto/Oto.mesh.xml");
-//    player.setLocalScale(0.5f);
-//    rootNode.attachChild(player);
-// 
-//    control = player.getControl(AnimControl.class);
-//    control.addListener(this);
-//    channel = control.createChannel();
-//    channel.setAnim("stand");
-  }
- 
-  public void onAnimCycleDone(AnimControl control, AnimChannel channel, String animName) {
-    if (animName.equals("Walk")) {
-      channel.setAnim("stand", 0.50f);
-      channel.setLoopMode(LoopMode.DontLoop);
-      channel.setSpeed(1f);
-    }
-  }
+		// animNames = control1.getAnimationNames().toArray(new String[0]);
+		channel1 = control1.createChannel();
 
-  public void onAnimChange(AnimControl control, AnimChannel channel, String animName) {
-    // unused
-  }
- 
-  /** Custom Keybinding: Map named actions to inputs. */
-  private void initKeys() {
-    inputManager.addMapping("Attack1", new KeyTrigger(KeyInput.KEY_0));
-    inputManager.addMapping("Attack2", new KeyTrigger(KeyInput.KEY_1));
-    inputManager.addMapping("Attack3", new KeyTrigger(KeyInput.KEY_2));
-    inputManager.addMapping("Backflip", new KeyTrigger(KeyInput.KEY_3));
-    inputManager.addMapping("Block", new KeyTrigger(KeyInput.KEY_4));
-    inputManager.addMapping("Climb", new KeyTrigger(KeyInput.KEY_5));
-    inputManager.addMapping("Crouch", new KeyTrigger(KeyInput.KEY_6));
-    inputManager.addMapping("Death1", new KeyTrigger(KeyInput.KEY_7));
-    inputManager.addMapping("Death2", new KeyTrigger(KeyInput.KEY_8));
-    inputManager.addMapping("HighJump", new KeyTrigger(KeyInput.KEY_9));
-    inputManager.addMapping("Idle1", new KeyTrigger(KeyInput.KEY_P));
-    inputManager.addMapping("Idle2", new KeyTrigger(KeyInput.KEY_O));
-    inputManager.addMapping("Idle3", new KeyTrigger(KeyInput.KEY_I));
-    inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_J));
-    inputManager.addMapping("JumpNoHeight", new KeyTrigger(KeyInput.KEY_K));
-    inputManager.addMapping("Kick", new KeyTrigger(KeyInput.KEY_L));
-    inputManager.addMapping("SideKick", new KeyTrigger(KeyInput.KEY_M));
-    inputManager.addMapping("Spin", new KeyTrigger(KeyInput.KEY_N));
-    inputManager.addMapping("Stealth", new KeyTrigger(KeyInput.KEY_B));
-    inputManager.addMapping("Walk", new KeyTrigger(KeyInput.KEY_V));
+		AnimControl control2 = model2.getControl(AnimControl.class);
+		channel2 = control2.createChannel();
 
-    inputManager.addListener(this, "Attack1","Attack2","Attack3","Backflip","Block",
-    		"Climb","Crouch","Death1","Death2","HighJump","Idle1","Idle2","Idle3","Jump",
-    		"JumpNoHeight","Kick","SideKick","Spin","Stealth","Walk");
-  }
- 
- // private ActionListener actionListener = new ActionListener() {
-    public void onAction(String name, boolean keyPressed, float tpf) {
- //   	System.out.print(name);
-//    	if (name.equals("Walk")&& !keyPressed  ){
+		SkeletonDebugger skeletonDebug = new SkeletonDebugger("skeleton1",
+				control1.getSkeleton());
+		Material mat = new Material(assetManager,
+				"Common/MatDefs/Misc/Unshaded.j3md");
+		mat.getAdditionalRenderState().setWireframe(true);
+		mat.setColor("Color", ColorRGBA.Green);
+		mat.getAdditionalRenderState().setDepthTest(false);
+		skeletonDebug.setMaterial(mat);
+		model1.attachChild(skeletonDebug);
 
-      //  if (!channel1.getAnimationName().equals("Walk")) {
-        	//channel1.setAnim("Walk", 0.50f);
-        	channel1.setAnim(name, 0.50f);
-        	channel1.setLoopMode(LoopMode.Loop);
-     //   }
-     // }
-    }
- // };
- 
+		skeletonDebug = new SkeletonDebugger("skeleton2",
+				control2.getSkeleton());
+		skeletonDebug.setMaterial(mat);
+		model2.attachChild(skeletonDebug);
+
+		rootNode.attachChild(model1);
+		rootNode.attachChild(model2);
+
+		// player = (Node) assetManager.loadModel("Models/Oto/Oto.mesh.xml");
+		// player.setLocalScale(0.5f);
+		// rootNode.attachChild(player);
+		//
+		// control = player.getControl(AnimControl.class);
+		// control.addListener(this);
+		// channel = control.createChannel();
+		// channel.setAnim("stand");
+	}
+
+	public void onAnimCycleDone(AnimControl control, AnimChannel channel,
+			String animName) {
+		if (animName.equals("Walk")) {
+			channel.setAnim("stand", 0.50f);
+			channel.setLoopMode(LoopMode.DontLoop);
+			channel.setSpeed(1f);
+		}
+	}
+
+	public void onAnimChange(AnimControl control, AnimChannel channel,
+			String animName) {
+		// unused
+	}
+
+	/** Custom Keybinding: Map named actions to inputs. */
+	private void initKeys() {
+		inputManager.addMapping("Attack1", new KeyTrigger(KeyInput.KEY_0));
+		inputManager.addMapping("Attack2", new KeyTrigger(KeyInput.KEY_1));
+		inputManager.addMapping("Attack3", new KeyTrigger(KeyInput.KEY_2));
+		inputManager.addMapping("Backflip", new KeyTrigger(KeyInput.KEY_3));
+		inputManager.addMapping("Block", new KeyTrigger(KeyInput.KEY_4));
+		inputManager.addMapping("Climb", new KeyTrigger(KeyInput.KEY_5));
+		inputManager.addMapping("Crouch", new KeyTrigger(KeyInput.KEY_6));
+		inputManager.addMapping("Death1", new KeyTrigger(KeyInput.KEY_7));
+		inputManager.addMapping("Death2", new KeyTrigger(KeyInput.KEY_8));
+		inputManager.addMapping("HighJump", new KeyTrigger(KeyInput.KEY_9));
+		inputManager.addMapping("Idle1", new KeyTrigger(KeyInput.KEY_P));
+		inputManager.addMapping("Idle2", new KeyTrigger(KeyInput.KEY_O));
+		inputManager.addMapping("Idle3", new KeyTrigger(KeyInput.KEY_I));
+		inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_J));
+		inputManager.addMapping("JumpNoHeight", new KeyTrigger(KeyInput.KEY_K));
+		inputManager.addMapping("Kick", new KeyTrigger(KeyInput.KEY_L));
+		inputManager.addMapping("SideKick", new KeyTrigger(KeyInput.KEY_M));
+		inputManager.addMapping("Spin", new KeyTrigger(KeyInput.KEY_N));
+		inputManager.addMapping("Stealth", new KeyTrigger(KeyInput.KEY_B));
+		inputManager.addMapping("Walk", new KeyTrigger(KeyInput.KEY_V));
+		inputManager.addMapping("Random", new KeyTrigger(KeyInput.KEY_SPACE));
+		inputManager.addListener(this, "Attack1", "Attack2", "Attack3",
+				"Backflip", "Block", "Climb", "Crouch", "Death1", "Death2",
+				"HighJump", "Idle1", "Idle2", "Idle3", "Jump", "JumpNoHeight",
+				"Kick", "SideKick", "Spin", "Stealth", "Walk", "Random");
+	}
+
+	// private ActionListener actionListener = new ActionListener() {
+	public void onAction(String name, boolean keyPressed, float tpf) {
+		// System.out.print(name);
+		if (name.equals("Random")) {
+			int p = 0;
+			p = (int) (Math.random() * animNumbers);
+			channel1.setAnim(animNames[p], 0.50f);
+			channel1.setLoopMode(LoopMode.Loop);
+			p = (int) (Math.random() * animNumbers);
+			channel2.setAnim(animNames[p], 0.50f);
+			channel2.setLoopMode(LoopMode.Loop);
+
+		} else {
+			// if (!channel1.getAnimationName().equals("Walk")) {
+			// channel1.setAnim("Walk", 0.50f);
+			channel1.setAnim(name, 0.50f);
+			channel1.setLoopMode(LoopMode.Loop);
+			// }
+		}
+	}
+	// };
+
 }
